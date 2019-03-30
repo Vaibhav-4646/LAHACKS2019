ffe64a0b89d3245db7f2ae723ba4192bbf3d5332
ish <ishnoors@gmail.com>
2019-03-29 23:02:24 -0700

    constants added

---
+++ constants.js
@@ -0,0 +1,2 @@
+export const TWITTER_KEY = "E2AYztI1jFmmiIerD9CQVuO4b";
+export const TWITTER_SECRET_KEY = "YxfpyTh19xyOcyBPkDsdP9SmnviQXRBMkkW3b28jpzWeN6ooON";
--- twitter.js
+++ twitter.js
@@ -1,1 +1,3 @@
 //this will be the final file
+import {TWITTER_KEY, TWITTER_SECRET_KEY} from './constants'
+const axios = require("axios")
